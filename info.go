951872540563d7beeb5eebf1feb61842e67d4e3e
cloxnu <o18089541818@outlook.com>
2021-03-15 14:55:35 +0800

    Create info

--- info.go
+++ info.go
@@ -1,6 +1,55 @@
 package main
 
 import (
-	"fmt"
 	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"sync"
 )
+
+var lock = &sync.Mutex{}
+
+type infoStruct struct {
+	LoadPath        string `yaml:"load_path"`
+	OutputDir       string `yaml:"output_dir"`
+	SearchNum       int    `yaml:"search_num"`
+	OutputImageSize int    `yaml:"output_image_size"`
+
+	Url       string `yaml:"url"`
+	UserAgent string `yaml:"user_agent"`
+
+	UrlFilter struct {
+		UrlRegex    string `yaml:"url_regex"`
+		NetlocRegex string `yaml:"netloc_regex"`
+		PathRegex   string `yaml:"path_regex"`
+	}
+
+	SiteSettings struct {
+		TitleRegex string `yaml:"title_regex"`
+	}
+}
+
+func (info *infoStruct) readConf() {
+	confFile, err := ioutil.ReadFile("info.yml")
+	if err != nil {
+		log.Println("Err: info file load err, ", err)
+	}
+	err = yaml.Unmarshal(confFile, info)
+	if err != nil {
+		log.Fatalln("Fatal: unmarshal, ", err)
+	}
+}
+
+var infoInstance *infoStruct
+
+func GetInfo() *infoStruct {
+	if infoInstance == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		if infoInstance == nil {
+			infoInstance = &infoStruct{}
+		}
+		infoInstance.readConf()
+	}
+	return infoInstance
+}
--- main.go
+++ main.go
@@ -3,11 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	a := 42
-	fmt.Println("Hello, World!", a)
-	for _, v := range "go"{
-		fmt.Println(string(v))
-	}
+	info := GetInfo()
+	fmt.Println(info)
 
 }
 
